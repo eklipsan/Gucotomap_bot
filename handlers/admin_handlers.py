c34e379ccd5723dd1d049fa2370380ac063a8b3b
Kirill <kshabalin2005@mail.ru>
2024-02-19 16:58:12 +0300

    Add logging to handlers/admin_handlers.py. Add get env/log commands

--- handlers/admin_handlers.py
+++ handlers/admin_handlers.py
@@ -1,27 +1,44 @@
 from aiogram import Router, F
-from aiogram.types import Message
+from aiogram.types import Message, FSInputFile
 from aiogram.filters import Command
 from config_data.config import Config, load_config
 from access_filters.tg_filter import IsAdmin
+from workers.logset import logger
 
 config: Config = load_config()
+filename = config.LogConfig.filepath
 
 router: Router = Router()
 router.message.filter(IsAdmin())
 
 
-@router.message(F.text == 'Am I admin?')
-async def send_answer_is_admin(message: Message):
-    await message.answer("Yes, you are admin.")
+@router.message(F.text == 'Get log')
+async def admin_get_log(message: Message):
+    user_id = message.from_user.id
+    document = FSInputFile(path=filename)
+    await message.answer_document(document=document)
+    logger.debug(f"User id {user_id} gets the log file {filename}")
+
+
+@router.message(F.text == 'Get env')
+async def admin_delete_log(message: Message):
+    user_id = message.from_user.id
+    document = FSInputFile(path='.env')
+    await message.answer_document(document=document)
+    logger.debug(f"User id {user_id} gets the env file")
 
 
 @router.message(Command('admin'))
 async def admin_show_manual(message: Message):
+    user_id = message.from_user.id
     manual_admin_message = (
         'List of commands that are available to you as an admin\n'
+        '<code>Get log</code> - return the log file\n'
+        '<code>Get env</code> - return the env file\n'
         '<code>Get answer</code> - show the right answer during the game\n\n'
         'Other commands, that are hidden, but available to everyone\n'
         '<code>Get user id</code> - show user\'s id'
     )
 
     await message.answer(manual_admin_message)
+    logger.debug(f"User id {user_id} gets the admin commands")
